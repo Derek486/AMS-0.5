f68f6ec0c411c17dc05219691739cd24f9c0021d
unknown <juanjosecondoripinto@gmail.com>
2024-10-17 20:19:03 -0500

    delete front

--- front-container/src/components/TableCell.jsx.jsx
+++
@@ -1,7 +0,0 @@
-import clsx from "clsx"
-
-export default function TableCell({ className, children }) {
-  return (
-    <div className={clsx(["px-6 py-4 text-sm whitespace-nowrap text-midnight font-normal truncate flex-1 flex items-center", className])}>{children}</div>
-  )
-}
--- front-flask-container/producer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-import json
-
-def send_to_kafka(broker, topic, message):
-  producer = KafkaProducer(bootstrap_servers=broker)
-  producer.send(topic, json.dumps(message).encode('utf-8'))
-  producer.flush()
