67ca0724e5b8e7b7ae67a72946cb319fead04513
unknown <juanjosecondoripinto@gmail.com>
2024-10-26 10:24:06 -0500

    delete kafka broker

---
+++ initial-aws.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum install -y docker
+sudo yum install -y libxcrypt-compat
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo yum install git -y
+git clone https://github.com/JuanJoseTCP/AMS-0.5.git
+cd AMS-0.5
--- kafka-broker/run_kafka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LOCAL_IP=192.168.0.7
-
-docker compose up -d
